f552a4dbd0935f2c4e209c03649e6fb431c6d658
Jordan Schneringer <schneringerj@gmail.com>
2022-06-02 15:38:08 -0700

    identifying variables and setting up functions in js

---
+++ Assets/js/script.js
@@ -0,0 +1,11 @@
+var currentquestion = 0;
+var timer = question.length * 15;
+var timer;
+
+var questions = document.getElementById("questions")
+var timer = document.getElementById("timer")
+var choices = document.getElementById("choices")
+var submit = document.getElementById("submit")
+var startButton = document.getElementById("startbutton")
+var initials = document.getElementById("initials")
+var feedback = document.getElementById("feedback")
